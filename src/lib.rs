df53c3bdf4ff05dfb4dc621f6efa1f7b3d529129
Maxiwell Luo <maxluoxiii@gmail.com>
2023-02-14 23:58:25 -0600

    Add ray struct (chapter 3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-use std::{
-    io::{BufWriter, Write},
-    ops,
-};
+pub mod types;
+
+use std::io::{BufWriter, Write};
+
+use types::Vec3;
 
 const PPM_HEADER: &str = "P3\n";
 
 pub struct Ppm {
     height: usize,
     width: usize,
-    pixels: Vec<Vec<(u32, u32, u32)>>,
+    pixels: Vec<Vec<Vec3>>,
 }
 
 impl Ppm {
@@ -25,7 +26,7 @@ impl Ppm {
         for y in 0..height {
             pixels.push(Vec::new());
             for _x in 0..width {
-                pixels[y].push((0, 0, 0));
+                pixels[y].push(Vec3::new());
             }
         }
         Ppm {
@@ -40,7 +41,7 @@ impl Ppm {
         while self.pixels.len() < self.height {
             let mut new_row = Vec::new();
             for _ in 0..self.width {
-                new_row.push((0, 0, 0));
+                new_row.push(Vec3::new());
             }
             self.pixels.push(new_row);
         }
@@ -54,13 +55,13 @@ impl Ppm {
                 row.truncate(self.width);
             } else {
                 while row.len() < self.width {
-                    row.push((0, 0, 0));
+                    row.push(Vec3::new());
                 }
             }
         }
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, pixel: (u32, u32, u32)) {
+    pub fn set_pixel(&mut self, x: usize, y: usize, pixel: Vec3) {
         self.pixels[self.height - 1 - y][x] = pixel;
     }
 
@@ -86,238 +87,19 @@ impl Ppm {
             .expect("Could not write max color");
 
         for (y, row) in self.pixels.iter().enumerate() {
-            for (x, (r, g, b)) in row.iter().enumerate() {
+            for (x, color) in row.iter().enumerate() {
                 writer
-                    .write_all(format!("{} {} {}\n", r, g, b).as_bytes())
+                    .write_all(
+                        format!(
+                            "{} {} {}\n",
+                            color.r() as u32,
+                            color.g() as u32,
+                            color.b() as u32
+                        )
+                        .as_bytes(),
+                    )
                     .expect(&format!("Could not write pixel {x} {y}"));
             }
         }
     }
-}
-
-pub struct Vec3 {
-    e: [f64; 3],
-}
-
-impl Vec3 {
-    pub fn new() -> Vec3 {
-        Vec3 { e: [0.0, 0.0, 0.0] }
-    }
-
-    pub fn from(e0: f64, e1: f64, e2: f64) -> Vec3 {
-        Vec3 { e: [e0, e1, e2] }
-    }
-
-    pub fn x(&self) -> f64 {
-        self.e[0]
-    }
-
-    pub fn y(&self) -> f64 {
-        self.e[1]
-    }
-
-    pub fn z(&self) -> f64 {
-        self.e[2]
-    }
-
-    pub fn r(&self) -> f64 {
-        self.e[0]
-    }
-
-    pub fn g(&self) -> f64 {
-        self.e[1]
-    }
-
-    pub fn b(&self) -> f64 {
-        self.e[2]
-    }
-
-    pub fn length(&self) -> f64 {
-        (self.e[0].powi(2) + self.e[1].powi(2) + self.e[2].powi(2)).sqrt()
-    }
-
-    pub fn squared_len(&self) -> f64 {
-        self.e[0].powi(2) + self.e[1].powi(2) + self.e[2].powi(2)
-    }
-
-    pub fn make_unit_vector(&mut self) {
-        let len = self.length();
-        self.e[0] /= len;
-        self.e[1] /= len;
-        self.e[2] /= len;
-    }
-
-    pub fn dot(&self, v2: &Vec3) -> f64 {
-        self.e[0] * v2.e[0] + self.e[1] * v2.e[1] + self.e[2] * v2.e[2]
-    }
-
-    pub fn cross(&self, v2: &Vec3) -> Vec3 {
-        Vec3 {
-            e: [
-                self.e[1] * v2.e[2] - self.e[2] * v2.e[1],
-                -(self.e[0] * v2.e[2] - self.e[2] * v2.e[0]),
-                self.e[0] * v2.e[1] - self.e[1] * v2.e[0],
-            ],
-        }
-    }
-}
-
-impl ops::Index<usize> for Vec3 {
-    type Output = f64;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.e[index]
-    }
-}
-
-impl ops::IndexMut<usize> for Vec3 {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.e[index]
-    }
-}
-
-impl ops::Add<Vec3> for Vec3 {
-    type Output = Self;
-
-    fn add(self, rhs: Vec3) -> Self::Output {
-        Self {
-            e: [
-                self.e[0] + rhs.e[0],
-                self.e[1] + rhs.e[1],
-                self.e[2] + rhs.e[2],
-            ],
-        }
-    }
-}
-
-impl ops::AddAssign<Vec3> for Vec3 {
-    fn add_assign(&mut self, rhs: Vec3) {
-        self.e[0] += rhs.e[0];
-        self.e[1] += rhs.e[1];
-        self.e[2] += rhs.e[2];
-    }
-}
-
-impl ops::Sub<Vec3> for Vec3 {
-    type Output = Self;
-
-    fn sub(self, rhs: Vec3) -> Self::Output {
-        Self {
-            e: [
-                self.e[0] - rhs.e[0],
-                self.e[1] - rhs.e[1],
-                self.e[2] - rhs.e[2],
-            ],
-        }
-    }
-}
-
-impl ops::SubAssign<Vec3> for Vec3 {
-    fn sub_assign(&mut self, rhs: Vec3) {
-        self.e[0] -= rhs.e[0];
-        self.e[1] -= rhs.e[1];
-        self.e[2] -= rhs.e[2];
-    }
-}
-
-impl ops::Mul<Vec3> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: Vec3) -> Self::Output {
-        Self {
-            e: [
-                self.e[0] * rhs.e[0],
-                self.e[1] * rhs.e[1],
-                self.e[2] * rhs.e[2],
-            ],
-        }
-    }
-}
-
-impl ops::MulAssign<Vec3> for Vec3 {
-    fn mul_assign(&mut self, rhs: Vec3) {
-        self.e[0] *= rhs.e[0];
-        self.e[1] *= rhs.e[1];
-        self.e[2] *= rhs.e[2];
-    }
-}
-
-impl ops::Mul<f64> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: f64) -> Self::Output {
-        Self {
-            e: [self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs],
-        }
-    }
-}
-
-impl ops::MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, rhs: f64) {
-        self.e[0] *= rhs;
-        self.e[1] *= rhs;
-        self.e[2] *= rhs;
-    }
-}
-
-impl ops::Div<Vec3> for Vec3 {
-    type Output = Self;
-
-    fn div(self, rhs: Vec3) -> Self::Output {
-        Self {
-            e: [
-                self.e[0] / rhs.e[0],
-                self.e[1] / rhs.e[1],
-                self.e[2] / rhs.e[2],
-            ],
-        }
-    }
-}
-
-impl ops::DivAssign<Vec3> for Vec3 {
-    fn div_assign(&mut self, rhs: Vec3) {
-        self.e[0] /= rhs.e[0];
-        self.e[1] /= rhs.e[1];
-        self.e[2] /= rhs.e[2];
-    }
-}
-
-impl ops::Div<f64> for Vec3 {
-    type Output = Vec3;
-
-    fn div(self, rhs: f64) -> Self::Output {
-        &self / rhs
-    }
-}
-
-impl ops::Div<f64> for &Vec3 {
-    type Output = Vec3;
-
-    fn div(self, rhs: f64) -> Self::Output {
-        Vec3 {
-            e: [self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs],
-        }
-    }
-}
-
-impl ops::DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, rhs: f64) {
-        self.e[0] /= rhs;
-        self.e[1] /= rhs;
-        self.e[2] /= rhs;
-    }
-}
-
-impl ops::Neg for Vec3 {
-    type Output = Self;
-
-    fn neg(self) -> Self::Output {
-        Self {
-            e: [self.e[0] * -1.0, self.e[1] * -1.0, self.e[2] * -1.0],
-        }
-    }
-}
-
-pub fn unit_vector(v: &Vec3) -> Vec3 {
-    v / v.length()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 use std::fs::File;
 
-use raytrace::{Ppm, Vec3};
+use raytrace::{Ppm, types::{Vec3, unit_vector, Ray}};
+
+fn bg_color(ray: Ray) -> Vec3 {
+    let unit_dir = unit_vector(&ray.direction);
+    let t = 0.5 * (unit_dir.y() + 1.0);
+    return (1.0 - t) * Vec3::from(1.0, 1.0, 1.0) + t * Vec3::from(0.5, 0.7, 1.0);
+}
 
 fn main() {
     let width = 200;
     let height = 100;
     let mut ppm = Ppm::from(width, height);
+
+    let lower_left_corner = Vec3::from(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::from(4.0, 0.0, 0.0);
+    let vertical = Vec3::from(0.0, 2.0, 0.0);
+    let origin = Vec3::from(0.0, 0.0, 0.0);
     for y in (0..height).rev() {
         for x in 0..width {
-            let col = Vec3::from(
-                (x as f64) / (width as f64),
-                (y as f64) / (height as f64),
-                0.2,
-            );
-            let ir = (col.r() * 255.99) as u32;
-            let ig = (col.g() * 255.99) as u32;
-            let ib = (col.b() * 255.99) as u32;
-            ppm.set_pixel(x, y, (ir, ig, ib));
+            let u = (x as f64) / (width as f64);
+            let v = (y as f64) / (height as f64);
+
+            let r = Ray::from(origin, lower_left_corner + u * horizontal + v * vertical);
+            let col = bg_color(r);
+
+            ppm.set_pixel(x, y, col * 255.99);
         }
     }
 
-    let mut file = File::create("output/chapter2.ppm").expect("Could not create ppm file");
+    let mut file = File::create("output/chapter3.ppm").expect("Could not create ppm file");
     ppm.write(&mut file);
 }
